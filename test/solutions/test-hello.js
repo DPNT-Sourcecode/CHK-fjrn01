552ff127ba68d44aa74e506fbc15c4813ff6e812
sbx_user1059 <sbx_user1059@ip-10-11-134-189.eu-west-2.compute.internal>
2018-04-10 10:45:46 +0000

    Tue Apr 10 10:45:46 UTC 2018

--- test/solutions/test-hello.js
+++ test/solutions/test-hello.js
@@ -1,6 +1,6 @@
 var hello = require('../../lib/solutions/hello');
 
 exports['say hello'] = function (test) {
-    test.equal(hello("anything"), "Hello, world");
+    test.equal(hello("friend"), "Hello, World!");
     test.done();
 };
