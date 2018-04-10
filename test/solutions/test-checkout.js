6e066b51283399b5092538861d01f023b474ba50
sbx_user1059 <sbx_user1059@ip-10-11-134-189.eu-west-2.compute.internal>
2018-04-10 10:53:29 +0000

    Tue Apr 10 10:53:29 UTC 2018

--- test/solutions/test-checkout.js
+++ test/solutions/test-checkout.js
@@ -11,7 +11,21 @@ var checkout = require('../../lib/solutions/checkout');
 +------+-------+----------------+
 */
 
-exports['checkout'] = function (test) {
+exports['checkout single items with correct price'] = function (test) {
     test.equal(checkout("A"), 50);
+    test.equal(checkout("B"), 30);
+    test.equal(checkout("C"), 20);
+    test.equal(checkout("D"), 15);
+    test.done();
+};
+
+exports['checkout invalid item gives price of -1'] = function (test) {
+    test.equal(checkout("ABC"), -1);
+    test.equal(checkout("Z"), -1);
+    test.equal(checkout("56"), -1);
+    test.equal(checkout(undefined), -1);
+    test.equal(checkout(null), -1);
+    test.equal(checkout(-50), -1);
+    test.equal(checkout(10), -1);
     test.done();
 };
